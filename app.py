5623394a51033501dec2b20e0669ad48cf09e718
Lucas da Silva de Sousa <lucas_sousa@sesisenaisp.org.br>
2024-07-04 16:45:05 -0300

    Pagina de chatbot criada

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, make_response, request
-import app
+from flask_cors import CORS
+import ia
 api = Flask(__name__)
-
+CORS(api)
 
 @api.route('/api/procurar', methods=['POST'])
 def search():  
     data = request.get_json()
     
-    response = app.retrieve_and_generate(data)
+    response = ia.retrieve_and_generate(data)
     return make_response(jsonify({'ChatBot': response}), 200)
 
 if __name__ == '__main__':
